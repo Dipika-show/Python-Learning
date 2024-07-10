7bf3ae1f1a4aad1fd0a8d679b465f7ee7d0de13b
Dipika Show <showdipika69@gmail.com>
2024-07-10 21:38:56 +0530

    list and tuple in python

---
+++ ch4problem3.py
@@ -0,0 +1,4 @@
+#check that tuple type cannot be changed in python
+a=(34,"dipika","banana")
+a[2]= "banana"
+print(a)
---
+++ ch4problem4.py
@@ -0,0 +1,3 @@
+#write a program to sum a list with 4 number
+a=[2,4,5,6,7]
+print(sum(a))
