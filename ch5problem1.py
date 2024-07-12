2497ccca0b71b7a646fa8af13a1fc24bac68477a
Dipika Show <showdipika69@gmail.com>
2024-07-12 22:14:26 +0530

    dict and set iin python

---
+++ ch5problem1.py
@@ -0,0 +1,10 @@
+#write a program to create a dictionary in bengali words with values as their english translation. provide user with an option to look is up
+
+words={
+    "khabar" :"food",
+    "kukur" : "dog",
+    "biral":"cat"
+}
+
+name=input("enter the word you want meaning of:")
+print(words[name])
---
+++ ch5problem3.py
@@ -0,0 +1,20 @@
+#create a empty dictionary. allow 4 friends to enter their favourite language as a value and use key as their names. assume that the names are unique
+ 
+d={}
+name =input("enter friends name:")
+lang=input("enter language name:")
+
+d.update({name:lang})
+name =input("enter friends name:")
+lang=input("enter language name:")
+
+d.update({name:lang})
+name =input("enter friends name:")
+lang=input("enter language name:")
+
+d.update({name:lang})
+name =input("enter friends name:")
+lang=input("enter language name:")
+
+d.update({name:lang})
+
